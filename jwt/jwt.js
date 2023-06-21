d54c2a3f2b63629d354ae8918d6f682add45a33c
keerthanac <keerthanachinna97@gmail.com>
2023-06-21 12:51:33 +0530

    jwt authentication in node js

---
+++ jwt/jwt.js
@@ -0,0 +1,5 @@
+- Creating Node.js authentication middleware
+- Using refresh tokens with JWT
+- Authenticating users with JWT
+- Invalidating refresh tokens to log users out
+- How to use JWT securely
