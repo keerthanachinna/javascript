9e53a892f9a5c80f02a1b446fe9ecb7d67364060
keerthanac <keerthanachinna97@gmail.com>
2023-07-18 14:22:38 +0530

    simple query systax and js imporatnt topic

---
+++ JavaScript.vb
@@ -0,0 +1,51 @@
+JavaScript 
+Roadmap
+1.	Js basics and syntaxes
+Variables 
+Equality operators(==/===)
+Operators Conditional Statements
+Loops
+Scopes
+
+2.	Js function
+Functions
+Function scope
+Closure
+Side Effects
+Callback Functions
+Recursion
+Generator Functions
+3.	This keyword
+4.	Data structure
+  Array and its methods
+Array destructing
+Objects and its methods
+Objects Inheritance
+Objects Destruction
+
+5.	Exception Handling
+6.	Object oriented programming
+Encapsulation
+Inheritance
+Polymorphism
+Abstaction
+_proto(Prototype)
+Classes
+7.	Js iterations
+8.	Js import and export.
+9.	Events in js
+Event Listeners
+Event Objects
+Event Bubbling
+10.	Asynchronous js
+JS sync vs async
+JS Promises
+JS Promises Chain
+JS async and await
+JS Promises API
+
+11.	Js memory management
+12.	Js debouncing and Throttling
+13.	Learn frameworks.
+
+
